1183c0372b9f91d9c5755fe38f6e78c1cc9f2dd4
ivanaolortegui <olorteguimor@gmail.com>
2019-10-07 13:00:37 -0500

    Creando Modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphics1Component } from './pages/graphics1/graphics1.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
-const routes: Routes = [
-  { path: '',
-  component: PagesComponent,
-  children: [
-    { path: 'dashboard', component: DashboardComponent},
-    { path: 'progress', component: ProgressComponent},
-    { path: 'grafica1', component: Graphics1Component},
-    { path: '', redirectTo: '', pathMatch: 'full'},
-  ]
 
-},
-
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: '**', component: NopagefoundComponent},
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', component: NopagefoundComponent }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true } )],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// Rutas
+import { APP_ROUTES } from './app-routing.module';
+
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphics1Component } from './pages/graphics1/graphics1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
@@ -19,24 +17,12 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graphics1Component,
-
-    HeaderComponent,
-
-    SidebarComponent,
-
-    BreadcrumbsComponent,
-
-    PagesComponent,
-
-    RegisterComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
